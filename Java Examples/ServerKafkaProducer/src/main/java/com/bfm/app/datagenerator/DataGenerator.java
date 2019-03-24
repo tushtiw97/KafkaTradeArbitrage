2c7a9da54a98174f1273f5779c06d5da75b058a8
Tushar Udayan Tiwar <tiwaritusharudayan@gmail.com>
2019-03-24 10:59:37 +0530

    Added the updated logic in DataGenerator for having buy/sell prices and quantities

--- Java Examples/ServerKafkaProducer/src/main/java/com/bfm/app/datagenerator/DataGenerator.java
+++ Java Examples/ServerKafkaProducer/src/main/java/com/bfm/app/datagenerator/DataGenerator.java
@@ -23,18 +23,152 @@ public class DataGenerator {
 			Integer randInt = random.nextInt(5) + 5;
 			
 			if(i%randInt == 0) {
-				jsonObject.put("nseBuyPrice", random.nextInt(100) + 300);
-				jsonObject.put("nseSellPrice", random.nextInt(100) + 300);
-				jsonObject.put("bseBuyPrice", random.nextInt(100) + 300);
-				jsonObject.put("bseSellPrice", random.nextInt(100) + 300);
+				
+				JSONArray bseBuyPrices = new JSONArray();
+				bseBuyPrices.put(random.nextInt(100) + 300);
+				bseBuyPrices.put(random.nextInt(100) + 300);
+				bseBuyPrices.put(random.nextInt(100) + 300);
+				bseBuyPrices.put(random.nextInt(100) + 300);
+				bseBuyPrices.put(random.nextInt(100) + 300);
+				
+				JSONArray bseSellPrices = new JSONArray();
+				bseSellPrices.put(random.nextInt(100) + 300);
+				bseSellPrices.put(random.nextInt(100) + 300);
+				bseSellPrices.put(random.nextInt(100) + 300);
+				bseSellPrices.put(random.nextInt(100) + 300);
+				bseSellPrices.put(random.nextInt(100) + 300);
+				
+				JSONArray nseBuyPrices = new JSONArray();
+				nseBuyPrices.put(random.nextInt(100) + 300);
+				nseBuyPrices.put(random.nextInt(100) + 300);
+				nseBuyPrices.put(random.nextInt(100) + 300);
+				nseBuyPrices.put(random.nextInt(100) + 300);
+				nseBuyPrices.put(random.nextInt(100) + 300);
+				
+				JSONArray nseSellPrices = new JSONArray();
+				nseSellPrices.put(random.nextInt(100) + 300);
+				nseSellPrices.put(random.nextInt(100) + 300);
+				nseSellPrices.put(random.nextInt(100) + 300);
+				nseSellPrices.put(random.nextInt(100) + 300);
+				nseSellPrices.put(random.nextInt(100) + 300);
+				
+				jsonObject.put("bseBuyPrices", bseBuyPrices);
+				jsonObject.put("bseSellPrices", bseSellPrices);
+				jsonObject.put("nseBuyPrices", nseBuyPrices);
+				jsonObject.put("nseSellPrices", bseSellPrices);
+				
+				JSONArray bseBuyQty = new JSONArray();
+				bseBuyQty.put(random.nextInt(10) + 1);
+				bseBuyQty.put(random.nextInt(10) + 1);
+				bseBuyQty.put(random.nextInt(10) + 1);
+				bseBuyQty.put(random.nextInt(10) + 1);
+				bseBuyQty.put(random.nextInt(10) + 1);
+				
+				JSONArray nseBuyQty = new JSONArray();
+				nseBuyQty.put(random.nextInt(10) + 1);
+				nseBuyQty.put(random.nextInt(10) + 1);
+				nseBuyQty.put(random.nextInt(10) + 1);
+				nseBuyQty.put(random.nextInt(10) + 1);
+				nseBuyQty.put(random.nextInt(10) + 1);
+				
+				JSONArray bseSellQty = new JSONArray();
+				bseSellQty.put(random.nextInt(10) + 1);
+				bseSellQty.put(random.nextInt(10) + 1);
+				bseSellQty.put(random.nextInt(10) + 1);
+				bseSellQty.put(random.nextInt(10) + 1);
+				bseSellQty.put(random.nextInt(10) + 1);
+				
+				JSONArray nseSellQty = new JSONArray();
+				nseSellQty.put(random.nextInt(10) + 1);
+				nseSellQty.put(random.nextInt(10) + 1);
+				nseSellQty.put(random.nextInt(10) + 1);
+				nseSellQty.put(random.nextInt(10) + 1);
+				nseSellQty.put(random.nextInt(10) + 1);
+				
+				jsonObject.put("bseBuyQty", bseBuyQty);
+				jsonObject.put("bseSellQty", bseSellQty);
+				jsonObject.put("nseBuyQty", nseBuyQty);
+				jsonObject.put("nseSellQty", nseSellQty);
+				
+//				jsonObject.put("nseBuyPrice", random.nextInt(100) + 300);
+//				jsonObject.put("nseSellPrice", random.nextInt(100) + 300);
+//				jsonObject.put("bseBuyPrice", random.nextInt(100) + 300);
+//				jsonObject.put("bseSellPrice", random.nextInt(100) + 300);
 			}
 			else {
 				Integer buy = random.nextInt(100) + 300;
 				Integer sell = random.nextInt(100) + 300;
-				jsonObject.put("nseBuyPrice", buy);
-				jsonObject.put("nseSellPrice", sell);
-				jsonObject.put("bseBuyPrice", buy);
-				jsonObject.put("bseSellPrice", sell);
+				
+				JSONArray bseBuyPrices = new JSONArray();
+				bseBuyPrices.put(buy);
+				bseBuyPrices.put(buy + 4);
+				bseBuyPrices.put(buy + 6);
+				bseBuyPrices.put(buy + 8);
+				bseBuyPrices.put(buy + 10);
+				
+				JSONArray bseSellPrices = new JSONArray();
+				bseSellPrices.put(sell);
+				bseSellPrices.put(sell + 4);
+				bseSellPrices.put(sell + 6);
+				bseSellPrices.put(sell + 8);
+				bseSellPrices.put(sell + 10);
+				
+				JSONArray nseBuyPrices = new JSONArray();
+				nseBuyPrices.put(buy);
+				nseBuyPrices.put(buy + 4);
+				nseBuyPrices.put(buy + 6);
+				nseBuyPrices.put(buy + 8);
+				nseBuyPrices.put(buy + 10);
+				
+				JSONArray nseSellPrices = new JSONArray();
+				nseSellPrices.put(sell);
+				nseSellPrices.put(sell + 4);
+				nseSellPrices.put(sell + 6);
+				nseSellPrices.put(sell + 8);
+				nseSellPrices.put(sell + 10);
+				
+				jsonObject.put("bseBuyPrices", bseBuyPrices);
+				jsonObject.put("bseSellPrices", bseSellPrices);
+				jsonObject.put("nseBuyPrices", nseBuyPrices);
+				jsonObject.put("nseSellPrices", bseSellPrices);
+				
+				JSONArray bseBuyQty = new JSONArray();
+				bseBuyQty.put(random.nextInt(10) + 1);
+				bseBuyQty.put(random.nextInt(10) + 1);
+				bseBuyQty.put(random.nextInt(10) + 1);
+				bseBuyQty.put(random.nextInt(10) + 1);
+				bseBuyQty.put(random.nextInt(10) + 1);
+				
+				JSONArray nseBuyQty = new JSONArray();
+				nseBuyQty.put(random.nextInt(10) + 1);
+				nseBuyQty.put(random.nextInt(10) + 1);
+				nseBuyQty.put(random.nextInt(10) + 1);
+				nseBuyQty.put(random.nextInt(10) + 1);
+				nseBuyQty.put(random.nextInt(10) + 1);
+				
+				JSONArray bseSellQty = new JSONArray();
+				bseSellQty.put(random.nextInt(10) + 1);
+				bseSellQty.put(random.nextInt(10) + 1);
+				bseSellQty.put(random.nextInt(10) + 1);
+				bseSellQty.put(random.nextInt(10) + 1);
+				bseSellQty.put(random.nextInt(10) + 1);
+				
+				JSONArray nseSellQty = new JSONArray();
+				nseSellQty.put(random.nextInt(10) + 1);
+				nseSellQty.put(random.nextInt(10) + 1);
+				nseSellQty.put(random.nextInt(10) + 1);
+				nseSellQty.put(random.nextInt(10) + 1);
+				nseSellQty.put(random.nextInt(10) + 1);
+				
+				jsonObject.put("bseBuyQty", bseBuyQty);
+				jsonObject.put("bseSellQty", bseSellQty);
+				jsonObject.put("nseBuyQty", nseBuyQty);
+				jsonObject.put("nseSellQty", nseSellQty);
+				
+//				jsonObject.put("nseBuyPrice", buy);
+//				jsonObject.put("nseSellPrice", sell);
+//				jsonObject.put("bseBuyPrice", buy);
+//				jsonObject.put("bseSellPrice", sell);
 			}
 			
 			jsonArray.put(jsonObject);
